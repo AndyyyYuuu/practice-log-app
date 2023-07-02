9c7ad91f04aca2bcffcc94e87020b00475ae3baa
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-07-01 19:39:41 -0700

    Create Xcode project files

---
+++ Practice Log App/Practice Log App/Practice_Log_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_Log_AppApp.swift
+//  Practice Log App
+//
+//  Created by Andy Yu on 2023-07-01.
+//
+
+import SwiftUI
+
+@main
+struct Practice_Log_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
